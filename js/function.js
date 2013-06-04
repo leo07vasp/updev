733e9976c9160c9fab4c2039417e9594de55f86b
leonardo <leonardo@bis2bis.com.br>
2013-06-04 17:09:19 -0300

    header, section about

---
+++ js/function.js
@@ -0,0 +1,17 @@
+$(function() {
+	
+jQuery('.carousel').cycle({
+		fx:     'fade', 
+        speed:  'slow', 
+        timeout: 5000,
+        pager:  '#nav-banner',
+        fit: 1,
+        width: "fit"
+});
+
+
+jQuery('.openMenu').on("click", function(){
+
+});
+
+});
